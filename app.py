73c65489ef8e36a7743a995eb5bead8512febd29
JiahangOK <2973493373@qq.com>
2018-11-26 20:14:03 +0800

    Update server.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from sqlalchemy import MetaData
 from sqlalchemy import create_engine
 from sqlalchemy.orm import *
 
+HOST = "192.168.6.117"
 
 # 创建flask_sqlalchemy基于sqlite的实例db
 app = Flask(__name__)
@@ -46,12 +47,14 @@ class userInfoTable(userdb.Model):
     def __repr__(self):
         return 'table name is ' + self.username
 
+
 # 建立model类，用于创建table/model
 class trainerInfoTable(userdb.Model):
     __tablename__ = 'trainerInfo'
 
     trainer_name = userdb.Column(userdb.String, primary_key=True)
     trainer_intro = userdb.Column(userdb.String, primary_key=True)
+
     def __repr__(self):
         return 'table name is ' + self.username
 
@@ -69,9 +72,25 @@ def show_photo():
         if filename is None:
             pass
         else:
-            image_data = open(os.path.join('./userPicture/','%s' % filename), "rb").read()
+            image_data = open(os.path.join('./userPicture/', '%s' % filename), "rb").read()
             response = make_response(image_data)
             response.headers['Content-Type'] = 'image/jpeg'
+            return response
+    else:
+        pass
+
+
+# show video
+@app.route('/video', methods=['GET'])
+def show_video():
+    if request.method == 'GET':
+        filename = request.args.get('filename')
+        if filename is None:
+            pass
+        else:
+            video_data = open(os.path.join('./video/', '%s' % filename), "rb").read()
+            response = make_response(video_data)
+            response.headers['Content-Type'] = 'video/mp4'
             return response
     else:
         pass
@@ -89,7 +108,9 @@ def check_user():
             rootdir = obj.pictureUrl
             phonenum = obj.user_phone_number
             list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
-            info_dict = {"info":[]}
+            info_dict = {"trainer_info": [],
+                         "video_info": []}
+            # 遍历教练信息
             for i in range(0, len(list)):
                 picture_file_name = list[i]
                 # 图片文件
@@ -101,13 +122,27 @@ def check_user():
                     trainer_name = picture_file_name.split(".")[0]
                     obj = trainerInfoTable.query.filter_by(trainer_name=trainer_name).first()
                     trainer_intro = obj.trainer_intro
-                    print("trainer_name:",trainer_name)
-                    trainer_image_url = "http://10.0.2.2:8080/userPicture?filename="+phonenum+"/"+picture_file_name
-                    data = {"trainer_image_url": trainer_image_url,"trainer_name":trainer_name,"trainer_intro":trainer_intro}
-                    info_dict.get("info").append(data)
+                    print("trainer_name:", trainer_name)
+                    trainer_image_url = "http://" + HOST + ":8080/userPicture?filename=" + phonenum + "/" + picture_file_name
+                    data = {"trainer_image_url": trainer_image_url, "trainer_name": trainer_name,
+                            "trainer_intro": trainer_intro}
+                    info_dict.get("trainer_info").append(data)
+
+            # 遍历视频信息
+            video_rootdir = "./video"
+            list = os.listdir(video_rootdir)
+            for i in range(0, len(list)):
+                video_file_name = list[i]
+                # 视频文件
+                path = os.path.join(video_rootdir, video_file_name)
+                if os.path.isfile(path):
+                    video_name = video_file_name.split(".")[0]
+                    video_url = "http://" + HOST + ":8080/video?filename=" + video_file_name
+                    data = {"video_url": video_url, "video_name": video_name}
+                    info_dict.get("video_info").append(data)
 
             jsoninfo = json.dumps(info_dict)
-            print("jsoninfo:",jsoninfo)
+            print("jsoninfo:", jsoninfo)
             return jsoninfo
         else:
             return '1'
